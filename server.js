4a1b2cd9997f89782dd88dd777c8aed49e45d97c
Gloria <gloriacheung812@gmail.com>
2022-06-24 22:33:47 +0000

    rename route

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/users", usersRoutes(db));
-app.use("/widgets", widgetsRoutes(db));
+app.use(usersRoutes(db));
+app.use(widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
