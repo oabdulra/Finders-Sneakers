26f0d3b5c667b317f5d95a0401ee93ed375598dc
Gloria <gloriacheung812@gmail.com>
2022-06-24 22:35:55 +0000

    initial setup of routes for sneakers, mycollection, myfavs

--- routes/widgets.js
+++ routes/widgets.js
@@ -9,19 +9,82 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  // display info about app and team
+  router.get("/about", (req, res) => {
+    res.render("about", {userID: req.session.user_id});
   });
+
+  // display all sneaker ads
+  router.get("/sneakers", (req, res) => {
+    db.getAllSneakers(req.query)
+    .then(properties => {
+      const templateVars = {
+        properties: properties,
+        userID: req.session.user_id
+      }
+      res.render("sneakers", templateVars);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
+  });
+
+  // display individual sneaker ad
+  router.get("/sneakers/:id", (req, res) => {
+    db.getAllSneakers(req.query)
+    .then(properties => {
+      const templateVars = {
+        properties: properties,
+        userID: req.session.user_id
+      }
+      res.render("sneakers", templateVars);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
+  });
+
+  // display form to contact seller
+  router.get("/sneakers/:id/contact", (req, res) => {
+    res.render("sneakers_show_contact", {userID: req.session.user_id});
+  });
+
+  // display form to create new sneaker ad
+  router.get("/sneakers/new", (req, res) => {
+    res.render("sneakers_new", {userID: req.session.user_id});
+  });
+
+  // display user's own ads
+  router.get("/mycollection", (req, res) => {
+
+  });
+
+  // display user's favourited ads
+  router.get("/myfavs", (req, res) => {
+
+  });
+
+  // create new ad
+  router.post("/sneakers", (req, res) => {
+
+  });
+
+  // contact seller associated with sneaker ad
+  router.post("/sneakers/:id", (req, res) => {
+
+  });
+
+  // delete user's own ad from my collections
+  router.post("/mycollection/:id/delete", (req, res) => {
+
+  });
+
+  // add sneaker ad to favs collection
+  router.post("/myfavs", (req, res) => {
+
+  });
+
   return router;
 };
