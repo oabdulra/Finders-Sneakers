a8c7d6f53d4f409ba8c47ac2f38ee3faa2938aec
Gloria <gloriacheung812@gmail.com>
2022-06-24 13:58:27 +0000

    set routes to login, logout, register for user

--- routes/users.js
+++ routes/users.js
@@ -21,5 +21,69 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // display form to register
+  router.get("/register", (req, res) => {
+    res.render("register");
+  });
+
+  // create new user
+  router.post("/register", (req, res) => {
+    const user = req.body;
+    database.addUser(user)
+    .then(user => {
+      if (!user) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+      res.redirect("/sneakers");
+    })
+    .catch(e => res.send(e));
+  });
+
+  // display form to login
+  router.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  // check if user exists with given email
+  const login =  function(email) {
+    return database.getUserWithEmail(email)
+    .then(user => {
+      if (user.email) {
+        return user;
+      }
+      return null;
+    })
+    .catch(e => {
+      res.send(e);
+    });
+  }
+  exports.login = login;
+
+  // login to existing account
+  router.post("/login", (req, res) => {
+    const {email} = req.body;
+    login(email)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.redirect("/sneakers");
+      })
+      .catch(e => {
+        res.send(e);
+      })
+  });
+
+  // logout of account
+  router.post("/logout", (req, res) => {
+    req.session.userId = null;
+    res.redirect("/sneakers");
+  });
+
   return router;
 };
