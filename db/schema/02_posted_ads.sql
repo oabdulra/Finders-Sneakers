3b65926f71f888c621f2b5c6b8bf8374a5977bee
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-06-25 08:48:16 -0400

    added ads tables for db

---
+++ db/schema/02_posted_ads.sql
@@ -0,0 +1,19 @@
+-- Ads Table --
+
+
+DROP TABLE IF EXISTS posted_ads CASCADE;
+
+CREATE TABLE posted_ads (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) NOT NULL,
+
+  title VARCHAR(255) NOT NULL,
+  ad_photo VARCHAR(255) NOT NULL,
+  ad_description TEXT,
+  price INTEGER NOT NULL,
+  size_id INTEGER REFERENCES shoe_size(id) NOT NULL,
+  brand_id INTEGER REFERENCES shoe_brands(id) NOT NULL,
+  post_date DATE NOT NULL,
+
+  ad_sold BOOLEAN NOT NULL DEFAULT FALSE
+);
