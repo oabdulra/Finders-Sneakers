ec4749b32fac0e1ccbe6c8159b8638fb9053fca1
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-06-25 08:24:12 -0400

    added users table for db

---
+++ db/schema/01_schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posted_ads CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE posted_ads (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  title VARCHAR(255) NOT NULL,
+  ad_photo VARCHAR(255) NOT NULL,
+  ad_description TEXT,
+  post_date DATE NOT NULL ,
+
+  ad_sold BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  ad_owner INTEGER REFERENCES posted_ads(owner_id) NOT NULL,
+  ad_buyer INTEGER REFERENCES users(id) NOT NULL,
+  message TEXT
+);
+
+CREATE TABLE favourite_ads (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
