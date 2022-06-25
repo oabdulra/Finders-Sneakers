057dbdebce9c7d573687b3735c3206557df6333c
Gloria <gloriacheung812@gmail.com>
2022-06-25 01:35:19 +0000

    separate widgetroutes for seperate resources

--- server.js
+++ server.js
@@ -43,12 +43,16 @@ app.use(cookieSession({
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const sneakersRoutes = require("./routes/sneakers");
+const myCollectionRoutes = require("./routes/my-collection");
+const myFavsRoutes = require("./routes/my-favs");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use(usersRoutes(db));
-app.use(widgetsRoutes(db));
+app.use("/sneakers", sneakersRoutes(db));
+app.use("/mycollection", myCollectionRoutes(db));
+app.use("/myfavs", myFavsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
