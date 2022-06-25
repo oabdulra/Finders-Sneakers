8fd8c32e126505b52292e61bc386fb7319ae893b
Gloria <gloriacheung812@gmail.com>
2022-06-25 15:27:38 +0000

    add post routes for creating new ad and contacting seller

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -36,22 +36,49 @@ module.exports = (db) => {
 
   // display form to contact seller
   router.get("/:id/contact", (req, res) => {
-    res.render("sneakers_show_contact", {userID: req.session.user_id});
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.send({error: "error"});
+    }
+    const user = db.user;
+    res.render("sneakers_show_contact", {user: {id: user.id, email: user.email}});
   });
 
   // display form to create new sneaker ad
   router.get("/new", (req, res) => {
-    res.render("sneakers_new", {userID: req.session.user_id});
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.send({error: "error"});
+    }
+    const user = db.user;
+    res.render("sneakers_new", {user: {id: user.id, email: user.email}});
   });
 
   // create new ad
   router.post("/", (req, res) => {
-    //ADD CODE
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.send({error: "error"});
+    }
+    db.addNewSneaker({...req.body, owner_id: user_id})
+    .then(sneaker => {
+      res.redirect(`/${sneaker.id}`);
+    })
+    .catch(e => res.send(e));
   });
 
   // contact seller associated with sneaker ad
   router.post("/:id", (req, res) => {
-    //ADD CODE
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.send({error: "error"});
+    }
+    db.contactSeller({...req.body})
+    .then(() => {
+      alert("message sent to seller");
+      res.redirect("/");
+    })
+    .catch(e => res.send(e));
   });
 
   return router;
