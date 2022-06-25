e2c936329ab3bfad5751cca6cb7437b17ac0d694
Gloria <gloriacheung812@gmail.com>
2022-06-25 23:09:15 +0000

    add route to favourite an ad

--- routes/my-favs.js
+++ routes/my-favs.js
@@ -1,15 +1,43 @@
+/* OSAMA: need these functions from db.js :
+ - function getMyFavs(userId) ==> returns array of sneaker objects
+ - function getUserWithId(userId) ==> returns user object
+ - function addToMyFavs(userId) ==> add new sneaker to favs db
+ - function deleteFromMyFavs(sneakerObject) ==> remove sneaker from favs db
+*/
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
   // display user's favourited ads
   router.get("/", (req, res) => {
-    //ADD CODE
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.error("error");
+      return;
+    }
+    db.getMyFavs(user_id)
+      .then(myfavs => {
+        db.getUserWithId(user_id)
+          .then(user => {
+            res.render("my_favs", {myfavs, user});
+          })
+          .catch(e => res.send(e));
+      })
+      .catch(e => res.send(e));
   });
 
   // add sneaker ad to favs collection
   router.post("/", (req, res) => {
-    //ADD CODE
+    const user_id = req.session.user_id;
+    if (!user_id) {
+      res.error("error");
+      return;
+    }
+    db.addToMyFavs({...req.body, owner_id: user_id})
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(e => res.send(e));
   });
 
   // delete sneaker ad from favs collection
