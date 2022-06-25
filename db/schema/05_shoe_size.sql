dc4fb46ef53bb75748efb7502037793da54da335
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-06-25 17:35:59 -0400

    added seeds for ads table

--- db/schema/05_shoe_size.sql
+++ db/schema/05_shoe_size.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS shoe_size CASCADE;
 
 CREATE TABLE shoe_size (
   id SERIAL PRIMARY KEY NOT NULL,
-  gender CHAR NOT NULL,
-  size INT NOT NULL
+  size INT NOT NULL,
+  gender VARCHAR(255) NOT NULL
 )
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,4 @@
 -- Users table seeds --
 INSERT INTO users (name, email) VALUES ('Raiza' , 'raiza@raiza.com');
-INSERT INTO users (name, email) VALUES ('Gloria', 'gloria@gloria.com');
-INSERT INTO users (name, email) VALUES ('Osama', 'osama@osama.com');
-INSERT INTO users (name, email) VALUES ('Pedro', 'pedro@pedro.com');
-INSERT INTO users (name, email) VALUES ('Alvin', 'alvin@alvin.com');
-INSERT INTO users (name, email) VALUES ('Cheng', 'cheng@cheng.com');
+('Gloria', 'gloria@gloria.com'), ('Osama', 'osama@osama.com'), ('Pedro', 'pedro@pedro.com'), ('Alvin', 'alvin@alvin.com'), ( 'Cheng', 'cheng@cheng.com');
 
