ce493074d55ae2d1ef6bc9d1c17232399a24a519
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-06-25 09:47:19 -0400

    added seeds for users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds --
+INSERT INTO users (name, email) VALUES ('Raiza' , 'raiza@raiza.com');
+INSERT INTO users (name, email) VALUES ('Gloria', 'gloria@gloria.com');
+INSERT INTO users (name, email) VALUES ('Osama', 'osama@osama.com');
+INSERT INTO users (name, email) VALUES ('Pedro', 'pedro@pedro.com');
+INSERT INTO users (name, email) VALUES ('Alvin', 'alvin@alvin.com');
+INSERT INTO users (name, email) VALUES ('Cheng', 'cheng@cheng.com');
+
