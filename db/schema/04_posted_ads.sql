361992be43ac6df0d74d0894002afad21a34f5ce
Gloria <gloriacheung812@gmail.com>
2022-06-28 20:49:39 +0000

    add delete cascade for foreign keys

--- db/schema/04_posted_ads.sql
+++ db/schema/04_posted_ads.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS posted_ads CASCADE;
 
 CREATE TABLE posted_ads (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner_id BIGINT REFERENCES users(id) NOT NULL,
+  owner_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
 
   title VARCHAR(255) NOT NULL,
   ad_photo VARCHAR(255) NOT NULL,
   ad_description TEXT,
   price INTEGER NOT NULL,
-  size_id INTEGER REFERENCES shoe_size(id) NOT NULL,
-  brand_id INTEGER REFERENCES shoe_brands(id) NOT NULL,
+  size_id INTEGER REFERENCES shoe_size(id) ON DELETE CASCADE,
+  brand_id INTEGER REFERENCES shoe_brands(id) ON DELETE CASCADE,
   post_date DATE NOT NULL,
 
   ad_sold BOOLEAN NOT NULL DEFAULT FALSE
