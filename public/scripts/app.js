ac6cbe2c321b41bf0d751b42659aa62ad0cb997e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-27 20:28:11 -0700

    Use jQuery to toggle form when user clicks on down arrow. Toggles between showing and hiding the form.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
 // Client facing scripts here
+
+
+$(document).ready(function() {
+
+  // When user clicks on down arrow on Browse page, toggle form
+  $(".fa-solid").on("click", function() {
+    $(".browse-form").toggle();
+  });
+
+});
